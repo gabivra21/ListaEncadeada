5ba411c812ae637bdf912d6de449b0036f77c4fb
Gabriela Vieira Ramos <gabrielavieirar21@gmail.com>
2024-09-16 23:25:55 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,8 +2,16 @@ import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        ListaEncadeada lista = new ListaEncadeada();
+        ListaEncadeada lista = new ListaEncadeada(23);
 
+        lista.add(89);
+        lista.add(88);
+        lista.add(56);
+        System.out.println(lista.isEmpty());
+        lista.addPos(2,87);
+        lista.remove(2);
+        lista.remove(1);
+        lista.getNodePos(3);
 
     }
 }
